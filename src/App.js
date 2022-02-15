2fe4b94942f42bc6eb7361e9ccd2c6584b3cdaaa
zuev720 <zuev720@mail.ru>
2022-02-15 14:42:03 +0300

    Поправил Роутинг

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     return (
         <Provider store={store}>
             <div className={'App'}>
-                <Router basename={'ra-diploma'}>
+                <Router>
                     <Header/>
                     <Switch>
                         <Route path="/cart">
